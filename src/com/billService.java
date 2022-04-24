a567dc18c6a034f24bea24282785ebd452eba5cc
pasan <pasanhasalankaubayasena@gmail.com>
2022-04-24 19:49:16 +0530

    setparams

--- src/com/billService.java
+++ src/com/billService.java
@@ -1,17 +1,28 @@
 package com;
-
-import javax.ws.rs.GET; 
-import javax.ws.rs.Path; 
+import model.BillManagement;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces; 
 import javax.ws.rs.core.MediaType; 
-@Path("/Hello")
+@Path("/billmanagement")
 public class billService {
 
-	@GET
-	@Path("/") 
-	@Produces(MediaType.TEXT_PLAIN) 
-	public String hello() 
-	 { 
-	 return "Hello world."; 
-	 } 
+//	@GET
+	 
+//	@Produces(MediaType.TEXT_PLAIN) 
+//	public String hello(@PathParam("id") int id) 
+//	{ 
+//		BillManagement newbill = new BillManagement();
+//		
+//		newbill.insertBill(MonthlyPayment, , null, null)
+//	} 
+//	
+//	@POST
+//	@Path("/add/{MonthlyPayment}/{address}/{billId}/{dueDate}")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public String insertbilldata(@PathParam("id") int id) {
+//		return"";
+//	}
 }
